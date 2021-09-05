f07c7514e134d06b18551a22a5baf97ef3a71701
Kevin <kevcespedesarancibia87@gmail.com>
2021-09-05 11:57:10 -0400

    show price, quantity, tax and STATE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 const form = document.querySelector("#totalizer-form");
 const quantity = document.querySelector("#quantity-input");
 const price = document.querySelector("#price-input");
+const state = document.querySelector("#state");
 
 const quantity_output = document.querySelector("#show-quantity");
 const price_output = document.querySelector("#show-price");
 const total_price_output = document.querySelector("#show-total-price");
 const total_taxes_price_output = document.querySelector("#show-total-taxes-price");
+const state_output = document.querySelector("#show-state");
+
+var taxes = {
+    "UT":0.0665,
+    "NV":0.08,
+    "TX":0.0625,
+    "AL":0.04,
+    "CA":0.0825
+}
 
 form.addEventListener("submit",event=>{
     event.preventDefault()
@@ -16,4 +26,5 @@ form.addEventListener("submit",event=>{
     let total_taxes_price =  total_price * (1+taxe_defect)
     total_price_output.innerHTML = "Total: " + total_price
     total_taxes_price_output.innerHTML = "Total with taxes: " + total_taxes_price
+    state_output.innerHTML = "State: " + state.value
 })
