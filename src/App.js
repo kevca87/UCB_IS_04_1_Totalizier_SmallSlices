8402793e16162e60640cd35080e4987c46f03657
Kevin <kevcespedesarancibia87@gmail.com>
2021-09-05 11:38:32 -0400

    show quantity and price

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 const form = document.querySelector("#totalizer-form");
 const quantity = document.querySelector("#quantity-input");
+const price = document.querySelector("#price-input");
 const quantity_output = document.querySelector("#show-quantity");
+const price_output = document.querySelector("#show-price");
 
 form.addEventListener("submit",event=>{
     event.preventDefault();
     quantity_output.innerHTML = "Quantity: " + quantity.value;
+    price_output.innerHTML = "Price: " + price.value;
 })
